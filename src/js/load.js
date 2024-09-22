251920b9a48f99715e4635dd61e3a25ad42b2936
Phil Ganem <philipganem@gmail.com>
2024-09-22 10:09:59 -0400

    fixed docs

--- src/js/load.js
+++ src/js/load.js
@@ -1,40 +1,50 @@
 /**
  * All documents should have paths similar to those below:
- * Time sheets: 'assets/pdfs/time-example.pdf',
- * Team Documents: 'assets/pdfs/doc-example.pdf'
+ * Time sheets: 'assets/timesheets/time-example.xlsx',
+ * Team Documents: 'assets/deliverables/doc-example.pdf'
  */
-const PDF_FILES = [
-    //Time
-    'assets/pdfs/time-SP-TimeSheet Phil.xlsx',
-    'assets/pdfs/time-SP-TimeSheet Laxmi.xlsx',
-    'assets/pdfs/time-SP-TimeSheet Ethan.xlsx',
-    'assets/pdfs/time-SP-TimeSheet Dylan.xlsx',
-    'assets/pdfs/time-SP-TimeSheet Connor.xlsx',
+const DELIVERABLE_FILES = [
     //Docs
-    'assets/pdfs/doc-4UP-Week3.pdf',
-    'assets/pdfs/doc-4UP-Week4.pdf',
+    'assets/deliverables/doc-4UP_Week3.pdf',
+    'assets/deliverables/doc-4UP_Week4.pdf',
 ];
 
+const TIME_FILES = [
+    //Time
+    'assets/timesheets/time-SP_TimeSheet_Phil.xlsx',
+    'assets/timesheets/time-SP_TimeSheet_Laxmi.xlsx',
+    'assets/timesheets/time-SP_TimeSheet_Ethan.xlsx',
+    'assets/timesheets/time-SP_TimeSheet_Dylan.xlsx',
+    'assets/timesheets/time-SP_TimeSheet_Connor.xlsx',
+];
+
+const FILES = [
+    DELIVERABLE_FILES,
+    TIME_FILES,
+]
+
 /**
- * Grabs pdfs on page load and makes hyperlinks to them
+ * Grabs documents on page load and makes hyperlinks to them
  */
-function loadPDFs() {
+function loadFiles() {
     const timeContainer = document.getElementById('time-container');
     const docContainer = document.getElementById('doc-container');
-    PDF_FILES.forEach(pdf => {
-        const link = document.createElement('a');
-        const fileNameType = pdf.split('/').pop().split(".")[0].split("-"); // name
-        link.href = pdf; // Use relative path to the PDF file
-        link.target = '_blank'; // Open in new tab
-        link.textContent = fileNameType[1];
-        if(fileNameType[0] == "time"){
-            timeContainer.appendChild(link);
-        } else if (fileNameType[0] == "doc"){
-            docContainer.appendChild(link);
-        } else {
-            docContainer.appendChild(link);
-        }
+    FILES.forEach(type => {
+        type.forEach(file => {
+            const link = document.createElement('a');
+            const fileNameType = file.split('/').pop().split(".")[0].split("-"); // name
+            link.href = file; // Use relative path to file
+            link.target = '_blank'; // Open in new tab
+            link.textContent = fileNameType[1];
+            if(fileNameType[0] == "time"){
+                timeContainer.appendChild(link);
+            } else if (fileNameType[0] == "doc"){
+                docContainer.appendChild(link);
+            } else {
+                docContainer.appendChild(link);
+            }
+        })
     });
 }
 // Call function on page load
-window.onload = loadPDFs;
+window.onload = loadFiles;
